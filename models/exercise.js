d407cf8b0f642681ad6f563383d7e6b20dddff01
Ross White <rosswhite@outlook.com>
2021-07-02 10:41:21 +0100

    Exercise model added

---
+++ models/exercise.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    type: { type: String, required: true },
+    name: { type: String, required: true },
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number
+});
+
+const Exercise = mongoose.model('Execise', exerciseSchema);
+
+module.exports = Exercise;
