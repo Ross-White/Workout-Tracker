8a6feb4386d9c00799ac2c9c231ad141a9ca34c9
Ross White <rosswhite@outlook.com>
2021-07-03 16:40:29 +0100

    refactoring updateWorkout controller

--- controllers/updateWorkoutController.js
+++ controllers/updateWorkoutController.js
@@ -1,21 +1,9 @@
 const db = require('../models');
 
-const updateWorkoutController = async (req, res) => {
+const updateWorkoutController = async ({ body, params }, res) => {
     try {
-        const updatedWorkout = await db.Workout.findOneAndUpdate({ _id: req.params.id },
-            {
-                $push: {
-                    exercises: {
-                        "type": req.body.type,
-                        "name": req.body.name,
-                        "duration": req.body.duration,
-                        "distance": req.body.distance,
-                        "weight": req.body.weight,
-                        "reps": req.body.reps,
-                        "sets": req.body.sets
-                    }
-                }
-            },
+        const updatedWorkout = await db.Workout.findByIdAndUpdate(params.id,
+            { $push: { exercises: body }},
             { new: true });
         if (!updatedWorkout) {
             res.status(404).json({ message: 'No Workout!' });
