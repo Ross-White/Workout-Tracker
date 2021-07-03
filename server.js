dfdcadc29d30df08d242c1c824dadd6bceea7265
Ross White <rosswhite@outlook.com>
2021-07-03 16:01:24 +0100

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser: true});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
