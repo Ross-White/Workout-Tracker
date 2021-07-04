358cf28bf7778c56ac29ed2856a74553dd2c5394
Ross White <rosswhite@outlook.com>
2021-07-04 18:58:23 +0100

    update to getRange controller

--- controllers/getRangeController.js
+++ controllers/getRangeController.js
@@ -5,7 +5,7 @@ const getRangeController = async (req, res) => {
         const dbWorkouts = await db.Workout.aggregate([{
             $addFields: { totalDuration: { $sum: '$exercises.duration' }}
         }])
-        .sort({ _id: -1 })
+        .sort({ day: -1 })
         .limit(7);
         if (!dbWorkouts) {
             res.status(404).json({ message: 'No Workouts Found! ' });
